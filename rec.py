7c3adc175e398036b210ce0b3e6e3ea0a5bcaf98
HawkingRadiation42 <shrey.209303098@muj.manipal.edu>
2023-06-03 22:17:31 +0530

    working code

--- rec.py
+++ rec.py
@@ -1,12 +1,13 @@
 import os
 import cv2
 from ultralytics import YOLO
+from paddleocr import PaddleOCR
 
 def load_model(path):
     model = YOLO(path)
     return model
 
-def detect_license_plate(image_path, model):
+def detect_license_plate(image_path, model, ocr_model):
     img = cv2.imread(image_path)
 
     results = model(img)
@@ -22,19 +23,28 @@ def detect_license_plate(image_path, model):
             crop = img[r[1]:r[3], r[0]:r[2]]
 
             # Generate a unique filename using the index i
-            filename = f"roi_{i}_{os.path.basename(image_path)}"
+            # filename = f"roi_{i}_{os.path.basename(image_path)}"
+            filename  = "roi.jpg"
             roi_path = os.path.join(roi_folder, filename)
             cv2.imwrite(roi_path, crop)
-            text = ocr_model.ocr(roi_path)
-            print(f"License Plate Text: {text}")
 
+            print(f"Saved cropped image: {roi_path}")
+            for filename in os.listdir(roi_folder):
+                roi_image_path = os.path.join(roi_folder, filename)
+                text = ocr_model.ocr(roi_image_path)
+                # print("text")
+                print(f"License Plate Text for {filename}: {text}")
 
 if __name__ == "__main__":
+    # Perform OCR on the saved images
+    
     model_path = 'runs/detect/train3/weights/best.pt'
     image_folder = 'datasets/test/images'
 
     model = load_model(model_path)
+    ocr_model = PaddleOCR(lang='en')
+
     for filename in os.listdir(image_folder):
         if filename.endswith('.jpg') or filename.endswith('.png'):
             image_path = os.path.join(image_folder, filename)
-            detect_license_plate(image_path, model)
+            detect_license_plate(image_path, model, ocr_model)
--- recogntion.py
+++ recogntion.py
@@ -2,20 +2,10 @@ from matplotlib import pyplot as plt
 import cv2
 import numpy as np
 from PIL import Image
-import pytesseract
 import torch
-# from yolov5.models.yolo import Model
 from ultralytics import YOLO
-from paddleocr import PaddleOCR, draw_ocr # main OCR dependencies
+# from paddleocr import PaddleOCR, draw_ocr # main OCR dependencies
 import os 
-
-def load_model(path):
-    # ckpt = torch.load(path, map_location="cpu")
-    # model_yaml = ckpt['model'].yaml
-    # model_yaml['anchors'] = 3 # add 'anchors' key
-    model = YOLO(path)  # load a pretrained model (recommended for training)
-    # metrics = model.val()  # evaluate model performance on the validation set
-    return model
 
 def detect_license_plate(image_path, model):
     # Load image
@@ -24,35 +14,35 @@ def detect_license_plate(image_path, model):
     img_tensor = torch.from_numpy(img_rgb).float().permute(2, 0, 1).unsqueeze(0) / 255.0  # Convert to tensor
 
     # Inference
-    results = model(img)  # predict on an image
-    # results = model(img_tensor)
+    print("sdfsddssdsfs")
+    # results = model.predict(source = img, save=True)  # predict on an image
+    results = model(img_tensor)
     # Extract bounding boxes
+    print("1---------------------------------")
     boxes = results.xyxy[0].cpu().detach().numpy()
 
     # Filter boxes with label corresponding to 'license plate' if you've trained for other objects too
+    print("2--------------------------------")
     license_plates = [box for box in boxes if box[5] == 0]
-    ocr_model = PaddleOCR(lang='en')
+    # ocr_model = PaddleOCR(lang='en')
     # Assuming 'license_plates' now contains boxes around license plates, extract these regions
+    print("3:--------------------------------")
     for i, (x1, y1, x2, y2, conf, label) in enumerate(license_plates):
+        print("hellow")
         # Crop the license plate out of the original image
         license_plate_img = img[int(y1):int(y2), int(x1):int(x2)]
-        
-        # Convert image to grayscale
-        # gray = cv2.cvtColor(license_plate_img, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite("license_plate_" + str(i) + ".jpg", license_plate_img)
 
-        # Use Tesseract to perform OCR on the extracted license plate
-        result = ocr_model.ocr(license_plate_img)
-        for res in result:
-            print(f"License Plate Text {i + 1}: {res[1][0]}")
+    #     # Convert image to grayscale
+    #     # gray = cv2.cvtColor(license_plate_img, cv2.COLOR_BGR2GRAY)
+    #     result = ocr_model.ocr(license_plate_img)
+    #     for res in result:
+    #         print(f"License Plate Text {i + 1}: {res[1][0]}")
 
 
 if __name__ == "__main__":
     model_path = 'runs/detect/train3/weights/best.pt'
     image_path = 'datasets/test/images/image_0009_jpg.rf.69c3f5b1dc1af7c0741b11ea54dd49c8.jpg'
-    model = load_model(model_path)
-    results = model(image_path)  # predict on an image
-    # plt.imshow(results)
-    print("hello")
-    print(results)
-    # cv2.imwrite('plot.png', results)
-    # detect_license_plate(image_path, model)
+    model = YOLO(model_path)  # load a pretrained model (recommended for training)
+    detect_license_plate(image_path, model)
+    # results = model(image_path)  # predict on an image
